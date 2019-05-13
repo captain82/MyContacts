f0113d2ccf83976594527359b0e3137051c98741
Ankit Kumar <nktkumar82@gmail.com>
2019-05-13 23:43:52 +0530

    Initial commit

---
+++ app/src/main/java/com/captain/ak/mycontacts/Adapter/contactsRecyclerViewAdapter.kt
@@ -0,0 +1,72 @@
+package com.captain.ak.mycontacts.Adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.captain.ak.mycontacts.DataClass.ContactDTO
+import com.captain.ak.mycontacts.R
+
+class contactsRecyclerViewAdapter(items:List<ContactDTO>, val context: Context): RecyclerView.Adapter<contactsRecyclerViewAdapter.ViewHolder>() {
+
+    private var list:List<ContactDTO> = items
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return contactsRecyclerViewAdapter.ViewHolder(LayoutInflater.from(context).inflate(R.layout.contacts_single_layout, p0, false))
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+
+        p0.contactText.text =list[p1].name
+        if (list[p1].image!=null)
+        {
+            p0.contactText1.visibility = View.INVISIBLE
+            p0.contactImageView.visibility = View.VISIBLE
+            p0.contactImageView.setImageBitmap(list[p1].image)
+        }
+        else
+        {
+            p0.contactText1.visibility = View.VISIBLE
+            p0.contactImageView.visibility = View.INVISIBLE
+
+        }
+
+        if (p0.contactText.text.splitToSequence(" ").count().equals(2)) {
+            p0.contactText1.text = p0.contactText.text.splitToSequence(" ").elementAt(0).elementAt(0).toString() + p0.contactText.text.splitToSequence(" ").elementAt(1).elementAt(0).toString()
+        }
+        else{
+            p0.contactText1.text = p0.contactText.text.splitToSequence(" ").elementAt(0).elementAt(0).toString()
+
+        }
+
+
+
+
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val contactImageView = view.findViewById<ImageView>(R.id.contactImageView)
+
+        val contactText = view.findViewById<TextView>(R.id.contactText)
+
+        val contactText1 = view.findViewById<TextView>(R.id.contactText1)
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/captain/ak/mycontacts/DataClass/ContactDTO.kt
@@ -0,0 +1,12 @@
+package com.captain.ak.mycontacts.DataClass
+
+import android.graphics.Bitmap
+
+class ContactDTO {
+
+    var name = ""
+
+    var number = ""
+
+    var image : Bitmap? = null
+}
