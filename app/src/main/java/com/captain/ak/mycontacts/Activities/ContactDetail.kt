3aafe0ac155fe7fe5fd7be8a765fd717cd0a4d8b
Ankit Kumar <nktkumar82@gmail.com>
2019-05-17 22:01:35 +0530

    Detail layout fully completed

--- app/src/main/java/com/captain/ak/mycontacts/Activities/ContactDetail.kt
+++ app/src/main/java/com/captain/ak/mycontacts/Activities/ContactDetail.kt
@@ -3,6 +3,7 @@ package com.captain.ak.mycontacts.Activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.captain.ak.mycontacts.DataClass.ContactDTO
 import com.captain.ak.mycontacts.R
 
@@ -11,6 +12,9 @@ class ContactDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
+        supportActionBar!!.hide()
+        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor= resources.getColor(R.color.fafafa_color)
 
         val bundle = intent.extras
         val contactDTO = bundle!!.getParcelable<ContactDTO>("data")
